df0b688a219da49dcefe43efe939009459340f72
papu <p.k12sin@gmail.com>
2024-09-07 01:15:30 +0530

    episode-2

--- App.js
+++ App.js
@@ -1,41 +1,18 @@
-
-
-/**
- * 
- * <div id="parent">
- *  <div id="child">
- *      <h1> I am h1 tag</h1>
- *      <h2>I am h2 tag</h2>
- *  </div>
- * 
- * 
- *  <div id="child">
- *      <h1> I am h1 tag</h1>
- *      <h2>I am h2 tag</h2>
- *  </div>
- * 
- * </div>
- */
+import React from "react";
+import ReactDOM from 'react-dom';
 
 
 const parent = React.createElement("div",{id:"parent"},[
-    React.createElement("dev",{id:"child1"},[
-        React.createElement("h1",{},"I am h1 tag"),
+    React.createElement("div",{id:"child1"},[
+        React.createElement("h1",{},"React"),
         React.createElement("h2",{},"I am h2 tag")
     ]),
-    React.createElement("dev",{id:"child2"},[
+    React.createElement("div",{id:"child2"},[
         React.createElement("h1",{},"I am h1 tag"),
         React.createElement("h2",{},"I am h2 tag")
     ])
 ])
 
-
-
-// const heading = React.createElement(
-//     "h1", 
-//     {id:"heading"}, 
-//     "Hello world from React!"
-// );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 #Namaste React
+
+#Parcel
+-Dev Build
+-Local Server
+-HMR = Hot Module Replacement
+-File Watching Algorithm - written in c++
+-Caching - Faster builds
+-Image Optimization
+-Minification
+-Bundling
+-Compress
+-Consistent Hashing
+-Code Splitting
+-Differencial Bundling - support older browsers
+-Diagnostic
+-Error Handling
+-Https
+-Tree Shaking - remove unused code
+-Different dev and prod build
