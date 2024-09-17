8b708a31f77ae23ee152179fc17ca6d64f8ea43f
papu <p.k12sin@gmail.com>
2024-09-17 20:11:29 +0530

    episode-8(let's get classy)

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,33 @@
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-      <h2>This is Namaste React Web Series</h2>
-    </div>
-  );
-};
+import User from "./User";
+import UserClass from "./UserClass";
+import React from "react";
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // console.log("parrent constructor");
+  }
+
+  componentDidMount() {
+    // console.log("parrent component did mount called");
+    //api call
+  }
+
+  render() {
+    // console.log("parrent render");
+    return (
+      <div>
+        <h1>About</h1>
+        <h2>This is Namaste React Web Series</h2>
+        <UserClass
+          name={"papu kumar singh"}
+          location={"Bangalore"}
+          contact={"papu@gmail.com"}
+        />
+      </div>
+    );
+  }
+}
 
 export default About;
---
+++ src/components/UserClass.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInfo: {
+        name: "Dummy",
+        location: "default",
+      },
+    };
+    console.log("contractor called");
+  }
+
+  async componentDidMount() {
+    console.log("component did mount called");
+    //api call
+    const data = await fetch("https://api.github.com/users/papusingh");
+    const json = await data.json();
+
+    // console.log(json);
+
+    this.setState({
+      userInfo: json,
+    });
+  }
+
+  componentDidUpdate() {
+    console.log("component did update got called");
+  }
+
+  componentWillUnmount() {
+    console.log("component will unmounte called");
+  }
+
+  render() {
+    console.log("render is called");
+    const { name, location, avatar_url } = this.state.userInfo;
+    return (
+      <div className="user-card">
+        <img src={avatar_url}></img>
+        <h2>Name: {name}</h2>
+        <h3>Location: {location}</h3>
+        <h3>Contact: papu@gmail.com</h3>
+      </div>
+    );
+  }
+}
+
+export default UserClass;
